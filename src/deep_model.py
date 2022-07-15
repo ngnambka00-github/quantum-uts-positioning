973b25ac1f23142cb5ea28d2375a07d61b925c23
ngnambka00-github <nam.nv182698@sis.hust.edu.vn>
2022-07-16 00:08:56 +0700

    code split all file

---
+++ src/deep_model.py
@@ -0,0 +1,169 @@
+from copy import deepcopy
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+from quantum_model import QuantumCircuit, QuantumLayer
+
+# define global variable
+N_INPUTS = 4
+N_QUBITS = 2
+N_OUT=2
+N_SHOTS=512
+
+class MeanSquareError(nn.Module):
+    def __init__(self):
+        super(MeanSquareError, self).__init__()
+
+    def forward(self, A, B):
+        errA = torch.zeros(A.shape[0])
+        for i in range(A.shape[0]):
+            errA[i] = torch.linalg.norm(A[i] - B[i])
+
+        return errA.mean()
+
+
+class UTSDataset(Dataset):
+    def __init__(self, X_train, y_train):
+        self.n_samples = X_train.shape[0]
+        self.x_data = torch.from_numpy(X_train).float()
+        self.y_data = torch.from_numpy(y_train).float()
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+# define netword has quantum_layer
+class Net(nn.Module):
+    def __init__(self, quantum=True, num_classes=N_OUT):
+        super(Net, self).__init__()
+        self.init_weight()
+
+        self.quantum = quantum
+        self.fc_in = nn.Sequential(
+            nn.Linear(64, 64),
+            nn.BatchNorm1d(64),
+            torch.nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32),
+            torch.nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(32, N_INPUTS),
+            nn.BatchNorm1d(N_INPUTS),
+            torch.nn.ReLU(),
+            nn.Dropout(0.3),
+        )
+        self.classical = nn.Linear(N_INPUTS, num_classes)
+        
+        self.quantum_layer = QuantumLayer.apply
+        self.pre_out = nn.Linear(N_INPUTS * (2 ** N_QUBITS), N_INPUTS)
+        self.pre_bn = nn.BatchNorm1d(N_INPUTS)
+        self.fc_out = nn.Linear(N_INPUTS, num_classes)
+        
+
+    def init_weight(self):
+        for m in self.modules():
+          if m.__class__.__name__.startswith('Linear'):
+              torch.nn.init.xavier_normal_(m.weight)
+              m.bias.data.fill_(0.0001)
+
+    def forward(self, x):
+        if self.quantum:
+            x = self.fc_in(x) # vao 64 -> ra n_inputs
+            quantum_out = torch.cat([self.quantum_layer(x_i) for x_i in x])
+
+            x = self.pre_out(quantum_out.float())
+            x = F.relu(self.pre_bn(x))
+            x = F.relu(self.fc_out(x))
+        else:
+            x = self.fc_in(x)
+            x = self.classical(x)
+
+        return x
+
+def train_model_deep(model, criterion, optimizer, scheduler, dataloaders, dataset_sizes, save_model_path=None, num_epochs=25):
+    best_model_state = deepcopy(model.state_dict())
+    best_loss = 1000000.0
+    statistic_loss = {
+        "train": [], 
+        "val": []
+    }
+    
+    for epoch in range(num_epochs):
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'test']:
+            print(f"Epoch {epoch} -> phase {phase}: ")
+
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            count = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                count += 1
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    print(f"\tBatch: {count}/{phase}: ")
+                    
+                    start = time.time()
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    end = time.time()
+                    print(f"\tTime forward: {start - end}")
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        optimizer.zero_grad()
+
+                        start = time.time()
+                        loss.backward()
+                        end = time.time()
+                        print(f"\tTime backward: {start - end}")
+
+                        start = time.time()
+                        optimizer.step()
+                        end = time.time()
+                        print(f"\tTime update weight: {start - end}")
+
+                    print()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            statistic_loss[phase].append(epoch_loss)
+
+            # deep copy the model
+            if phase == 'test' and epoch_loss < best_loss:
+                best_loss = epoch_loss
+                best_model_state = deepcopy(model.state_dict())
+
+    # model.load_state_dict(best_model_state)
+    # save best_model_state
+    if not (save_model_path is None):
+        torch.save(best_model_state, save_model_path)
+
+    # return thong ke loss (train, val) | lowest_loss | best model state
+    return statistic_loss, best_loss, best_model_state
+
+
+# if __name__ == "__main__":
+#     net = Net(quantum=True)
+#     a = torch.rand((8, 64))
+#     out = net(a)
+#     out.shape
---
+++ src/quantum_model.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+
+import numpy as np
+import qiskit
+
+from qiskit.visualization import plot_state_qsphere,plot_histogram, plot_state_city
+import matplotlib.pyplot as plt
+
+# define global variable
+N_INPUTS = 4
+N_QUBITS = 2
+N_OUT=2
+N_SHOTS=512
+
+from qiskit.providers.aer.noise import NoiseModel
+from qiskit.providers.aer.noise.errors import pauli_error, depolarizing_error
+def get_noise(p):
+    error_meas = pauli_error([('X',p), ('I', 1 - p)])
+
+    noise_model = NoiseModel()
+    noise_model.add_all_qubit_quantum_error(error_meas, "measure") # measurement error is applied to measurements
+        
+    return noise_model
+
+
+class QuantumCircuit: 
+    """ 
+    This class provides a simple interface for interaction  with the quantum circuit 
+    """
+
+    def __init__(self, n_qubits, backend, shots, is_add_noise=False, n_inputs=N_INPUTS):
+        # -- Circuit definitaion --
+
+        self.n_qubits = n_qubits
+        self.n_inputs = n_inputs
+
+        self.circuit = qiskit.QuantumCircuit(n_qubits)
+        self.theta = qiskit.circuit.Parameter("theta")
+
+        all_qubits = [i for i in range(n_qubits)]
+        self.circuit.h(all_qubits)
+        self.circuit.barrier()
+        self.circuit.ry(self.theta, all_qubits)
+
+        self.circuit.measure_all()
+        # ------------------------
+
+        self.is_add_noise = is_add_noise
+        self.backend = backend
+        self.shots = shots
+
+    def forward(self, thetas):
+        t_qc = qiskit.transpile(self.circuit, self.backend)
+        q_obj = qiskit.assemble(t_qc, shots=self.shots, parameter_binds=[{self.theta: theta.item()} for theta in thetas])
+
+        # run simulator
+        noise_model = get_noise(0.01)
+        job = self.backend.run(q_obj, noise_model=noise_model if self.is_add_noise else None, shots=self.shots)
+        result = job.result().get_counts()
+            
+        exp = []
+        for dict_ in result:
+            counts = np.array(list(dict_.values()))
+            states = np.array([int(k, 2) for k in list(dict_.keys())])
+            prob = counts / self.shots
+            expectation = states * prob
+
+            exp.extend(expectation)
+
+        while len(exp) < self.n_inputs * (2 ** self.n_qubits):
+            exp.extend([0.00])
+        
+        return np.asarray(exp).T
+
+    
+    def plot(self, thetas):
+        self.plot_backend = qiskit.Aer.get_backend("statevector_simulator")
+        t_qc = qiskit.transpile(self.circuit, self.plot_backend)
+        q_obj = qiskit.assemble(t_qc, shots=self.shots, parameter_binds=[{self.theta: theta.item()} for theta in thetas])
+
+        job = self.plot_backend.run(q_obj)
+        result = job.result().get_counts()
+
+        # display show
+        # display(plot_state_qsphere(job.result().get_statevector()))
+        # display(plot_state_city(job.result().get_statevector(), figsize=[16, 9]))
+        # display(self.circuit.draw("mpl"))
+        # display(plot_histogram(job.result().get_counts()))
+
+
+class QuantumLayer(Function):
+    @staticmethod
+    def forward(ctx, inp):
+        if not hasattr(ctx, 'QiskitCirc'):
+            simulator = qiskit.Aer.get_backend('aer_simulator')
+            ctx.QiskitCirc = QuantumCircuit(N_QUBITS, simulator, shots=N_SHOTS, n_inputs=N_INPUTS, is_add_noise=True)
+
+        exp_value = ctx.QiskitCirc.forward(inp)
+        result = torch.tensor([exp_value])
+        ctx.save_for_backward(result, inp)
+        
+        return result
+    
+    @staticmethod
+    def backward(ctx, grad_output):
+        # gia tri ship nay co the thay doi
+        SHIFT = np.pi/2
+
+        forward_tensor, i = ctx.saved_tensors
+        input_numbers = i
+        gradients = torch.Tensor()
+        
+        for k in range(N_INPUTS):
+            shift_right = input_numbers.detach().clone()
+            shift_right[k] = shift_right[k] + SHIFT
+            shift_left = input_numbers.detach().clone()
+            shift_left[k] = shift_left[k] - SHIFT
+            
+            expectation_right = ctx.QiskitCirc.forward(shift_right)
+            expectation_left  = ctx.QiskitCirc.forward(shift_left)
+            gradient = torch.tensor([expectation_right]) - torch.tensor([expectation_left])
+            gradients = torch.cat((gradients, gradient.float()))
+        result = torch.Tensor(gradients)
+
+        return (result.float() * grad_output.float()).T
+
+
+# if __name__ == "__main__":
+#     x1 = torch.tensor([0.0624]*N_INPUTS, requires_grad=True)
+
+#     qc = QuantumLayer.apply
+#     y1 = qc(x1)
+#     print(y1)
+#     print('y1 after quantum layer: {}'.format(y1.shape))
+
+#     y1 = nn.Linear(2 ** N_QUBITS * N_INPUTS,1)(y1.float())
+#     y1.backward()
+
+#     print('x.grad = {}'.format(x1.grad))
